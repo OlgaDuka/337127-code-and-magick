39b4ecbad9da391bbe40e8b8bad2e249ffca8be6
Olga Duka <olgaduk@mail.ru>
2017-11-19 13:16:04 +0600

    Закруглила уголки холста

--- js/stat.js
+++ js/stat.js
@@ -2,11 +2,32 @@
 
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeRect(110, 20, 420, 270);
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.beginPath();
+  ctx.moveTo(110, 30);
+  ctx.quadraticCurveTo(110, 20, 120, 20);
+  ctx.lineTo(520, 20);
+  ctx.quadraticCurveTo(530, 20, 530, 30);
+  ctx.lineTo(530, 280);
+  ctx.quadraticCurveTo(530, 290, 520, 290);
+  ctx.lineTo(120, 290);
+  ctx.quadraticCurveTo(110, 290, 110, 280);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
+
   ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.beginPath();
+  ctx.moveTo(100, 20);
+  ctx.quadraticCurveTo(100, 10, 110, 10);
+  ctx.lineTo(510, 10);
+  ctx.quadraticCurveTo(520, 10, 520, 20);
+  ctx.lineTo(520, 270);
+  ctx.quadraticCurveTo(520, 280, 510, 280);
+  ctx.lineTo(110, 280);
+  ctx.quadraticCurveTo(100, 280, 100, 270);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
