a9965473c52d3398570c9a88ff6ba630023862ff
Olga Duka <olgaduk@mail.ru>
2017-11-26 18:35:00 +0600

    Навела порядок

--- js/setup.js
+++ js/setup.js
@@ -1,41 +1,30 @@
 'use strict';
+// Константы
 var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SUR_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLORS = [[101, 137, 164], [241, 43, 107], [146, 100, 161], [56, 159, 117], [215, 210, 55], [0, 0, 0]];
+var WIZARD_COAT_COLORS = ['101, 137, 164', '241, 43, 107', '146, 100, 161', '56, 159, 117', '215, 210, 55', '0, 0, 0'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var MAX_WIZARDS = 4;
+// Переменные
+var wizards = [];
+var userDialog = document.querySelector('.setup');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
 
-var getRandomValueForArr = function (arr) {
-  return Math.floor(Math.random() * (arr.length));
+// Функции
+// Получение случайного целого значения
+var getRandomInt = function (minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
 };
-
+// Получение имен магов
 var generateName = function () {
-  var indexRandom = getRandomValueForArr(WIZARD_FIRST_NAMES);
+  var indexRandom = getRandomInt(0, WIZARD_FIRST_NAMES.length);
   var firstName = WIZARD_FIRST_NAMES.splice(indexRandom, 1);
   var surName = WIZARD_SUR_NAMES.splice(indexRandom, 1);
   return firstName + ' ' + surName;
 };
-
-var generateColor = function (arrColor) {
-  var indexRandom = getRandomValueForArr(arrColor);
-  return arrColor.splice(indexRandom, 1);
-};
-
-var wizards = [];
-
-for (var i = 0; i < 4; i++) {
-  wizards[i] = {
-    name: generateName(),
-    coatColor: 'rgb(' + generateColor(WIZARD_COAT_COLORS) + ')',
-    eyesColor: generateColor(WIZARD_EYES_COLORS),
-  };
-}
-
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
+// Формирование фигурок магов для вывода на страницу - заполнение данными из массива объектов
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -44,10 +33,21 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+// Реализация
+// Заполняем данными массив объектов магов
+for (var i = 0; i < MAX_WIZARDS; i++) {
+  wizards[i] = {
+    name: generateName(),
+    coatColor: 'rgb(' + WIZARD_COAT_COLORS.splice(getRandomInt(0, WIZARD_COAT_COLORS.length), 1) + ')',
+    eyesColor: WIZARD_EYES_COLORS.splice(getRandomInt(0, WIZARD_EYES_COLORS.length), 1)
+  };
 }
+// Переносим данные из массива объектов во фрагмент с маркерами для вставки на страницу
+wizards.forEach(function (elem) {
+  fragment.appendChild(renderWizard(elem));
+});
+// Добавляем фрагмент на страницу
 similarListElement.appendChild(fragment);
-
+// Делаем видимым диалог
+userDialog.classList.remove('hidden');
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
